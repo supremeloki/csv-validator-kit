5710ce2a9bfd46d1dfb2c4bdad89ba68119e07c7
Kooroush Masoumi <kooroushmasoumi@gmail.com>
2024-01-30 20:13:18 +0330

    implement __init__ validation

--- src/csv_validator_kit/__init__.py
+++ src/csv_validator_kit/__init__.py
@@ -1,25 +1,2 @@
 from .core import (
     ColumnRule,
-    Row,
-    Schema,
-    SchemaDefinitionError,
-    ValidationError,
-    ValidationResult,
-    Violation,
-    validate_file,
-    validate_rows,
-)
-
-__all__ = [
-    "ColumnRule",
-    "Row",
-    "Schema",
-    "SchemaDefinitionError",
-    "ValidationError",
-    "ValidationResult",
-    "Violation",
-    "validate_file",
-    "validate_rows",
-]
-
-__version__ = "0.1.0"
